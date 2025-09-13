c3d8d2a5548111bd72d95b94d63ef3cbdcb4c130
marcp2006 <ma463652@ucf.edu>
2025-09-12 22:26:13 -0400

    Add files via upload

--- vm.c
+++ vm.c
@@ -36,6 +36,7 @@ Due Date: Friday, September 12th, 2025
 // defines variables
 int pas[500];
 int PC, BP, SP;
+PC = 499;
 struct InstructionRegister {
     int OP;
     int L;
@@ -47,16 +48,50 @@ int base(int BP, int L);
 void printStack(int PC, int BP, int SP, int pas);
 
 int main() {
-    struct InstructionRegister IR;
-    
+    struct InstructionRegister IR; // sets up struct
 
+    // reads input and opens
+    FILE * input = fopen(argv[1], "r");
+
+    // if unable to find file, terminates program
+    if (input == NULL) {
+        printf("Unable to open file.\n");
+        return 0;
+    }
+
+    // indexes all elements to 0
     for (int i = 0; i < 500; i++) {
         pas[i] = 0;
     }
 
-    IR.OP = pas[PC];
-    IR.L = pas[PC - 1];
-    IR.M = pas[PC - 2];
+    // temp variables
+    int op;
+    int l;
+    int m;
+    int index = 499;
+
+    while (fscanf(fp, "%d %d %d", &op, &l, &m) == 3) {
+        pas[index] = op;
+        pas[index - 1] = l;
+        pas[index - 2] = m;
+        index = index - 3;
+    }
+
+    fclose(input);
+
+    int flag = 0; // flag for halt operation
+
+    while (flag == 0) {
+        // fetch cycle
+        IR.OP = pas[PC];
+        IR.L = pas[PC - 1];
+        IR.M = pas[PC - 2];
+        PC = PC - 3;
+
+        if (IR.OP == 1) {
+            
+        }
+    }
 
     return 0;
 }
